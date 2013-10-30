2512d1c97dc9c2d8bfef750e731046c0691d9ade
Nikita Popov <losnikitos@gmail.com>
2013-10-30 20:51:36 +0400

    player pictures added

--- js/players.js
+++ js/players.js
@@ -1,11 +1,13 @@
 var Team = function () {
     this.populateIn = function (ul) {
-
-        $.getJSON("data/players.json", function (data) {
+        $.getJSON("data/players.json", function (data)
+         {
             data.forEach(function (player) {
-                    var li = $('<li/>');
-                    li.html(player.name);
-                    li.addClass('player');
+                    var img = $('<img/>').attr("src", "img/players/melnik.jpg");
+                    var div = $('<div/>').html(player.name);
+
+                    var li = $('<li/>').addClass('player').append(img).append(div);
+
                     ul.append(li);
                 }
             )
